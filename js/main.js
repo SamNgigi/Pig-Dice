45e9287af51bb41c05730d16b991364c77610eb9
Sam Ngigi <sam.t.ngigi@gmail.com>
2017-11-30 04:55:49 +0300

    Active Player still buggy

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
  var held_score = 0;
  var active_player = 1;
  var game_active = true;
+ var score1 = parseInt($("#score-1").text());
 // our dice images
 
 /*
@@ -28,20 +29,52 @@ var diceimgs = [
 
 // Game State function.
 if(game_active){
-  //1. Random Number
+  //1. Random Number.
   var dice_face = Math.floor(Math.random()*6)+1;
+
+  //2. Display dice result.
+  var diceFace = $("#diceFaces").attr("src", diceimgs[dice_face-1]).appendTo("#diceFaces");
+  //3. Display result in current active player box.
+  if(active_player === 1 && dice_face != 1 && score1<100){
+    held_score += dice_face;
+     $("#activeScore1").text(held_score);
+
+  } else if (active_player === 2 && dice_face != 1){
+    $("#activeScore2").text(dice_face);
+  }else {
+    alert("You Rolled One");
+  }
 }
+
 
 // FRONT END
 $(document).ready(function() {
   //click function for "Roll Dice" button
   $("#roll-dice").click(function() {
-     alert("Roll dice button Working! You rolled " + dice_face + ".");
+    active_player;
+    var dice_face = Math.floor(Math.random()*6)+1;
+    //  alert("Roll dice button Working! You rolled " + dice_face + ".");
     //  $("#theDice").prepend('<img id="#dice-faces" src="images/Dice/two.png" />')
     //Below code links Jquery to images
     // var diceFace = $("#diceFaces").attr("src", "images/Dice/two.png");
     //Below is links the dice image to the random number.
+    // var diceFace = $("#diceFaces").attr("src", diceimgs[dice_face-1]).appendTo("#diceFaces");
+    // Below code links random number to active current score
+    // $("#activeScore1").text(dice_face);
+    // alert(score1);
     var diceFace = $("#diceFaces").attr("src", diceimgs[dice_face-1]).appendTo("#diceFaces");
+
+    if(active_player === 1 && dice_face != 1 && score1<100){
+      held_score += dice_face;
+       $("#activeScore1").text(held_score);
+
+    } else if (active_player === 2 && dice_face != 1){
+      $("#activeScore2").text(dice_face);
+    }else {
+      alert("You Rolled One");
+    }
+
+
   })
   //click function for "New Game" button
   $("#new-game").click(function () {
@@ -50,9 +83,11 @@ $(document).ready(function() {
   // click function for player1 "Hold" button.
   $("#hold1").click(function () {
     alert("Hold player1 button Working!");
+    active_player === 2;
   })
   // click function for player2 "Hold" button.
   $("#hold2").click(function () {
     alert("Hold player2 button Working!");
+    active_player === 1;
   })
 });
