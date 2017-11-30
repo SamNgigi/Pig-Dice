16fe2556217fe120277d77db6f4bcde51c7b29db
Sam Ngigi <sam.t.ngigi@gmail.com>
2017-11-30 18:08:13 +0300

    Added Roll Message js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,53 @@
 // Global-variables. These are the default variables of our variables
- var scores = [0,0];
- var held_score = 0;
- var active_player = 1;
- var game_active = true;
- var score1 = parseInt($("#score-1").text());
+var scores = [0, 0];
+var held_score = 0;
+var active_player = 1;
+var game_active = true;
+var score1 = parseInt($("#score-1").text());
 
 // our dice images
 var diceimgs = [
-     "images/Dice/one.png",
-     "images/Dice/two.png",
-     "images/Dice/three.png",
-     "images/Dice/four.png",
-     "images/Dice/five.png",
-     "images/Dice/six.png"
+  "images/Dice/one.png",
+  "images/Dice/two.png",
+  "images/Dice/three.png",
+  "images/Dice/four.png",
+  "images/Dice/five.png",
+  "images/Dice/six.png"
 ];
 
-// TODO: Start with all the functions that we will use in the program
-var disableButton = function (btn, time) {
+// TODO: Disables roll button
+                //the button , the duration of disabling
+var disableButton = function(btn, time) {
   //disable roll
   btn.disabled = true;
-  setTimeout(function () {btn.disabled = false;},time)
+  setTimeout(function() {
+    btn.disabled = false;
+  }, time)
 }
+// TODO: Roll message hide.
+var rollMessage = function functionName() {
+  $("#overlay"+active_player).show()
+}
+
 // Game State function.
-if(game_active){
+if (game_active) {
   //1. Random Number.
-  var dice_number = Math.floor(Math.random()*6)+1;
+  var dice_number = Math.floor(Math.random() * 6) + 1;
 
   //2. Display dice result.
-  var diceFace = $("#diceFaces").attr("src", diceimgs[dice_number-1]).appendTo("#diceFaces");
+  var diceFace = $("#diceFaces").attr("src", diceimgs[dice_number - 1]).appendTo("#diceFaces");
   //3. Display result in current active player box.
-  if(dice_number !== 1){
+  if (dice_number !== 1) {
     //Add the dice numbers to current scores if one is not rolled
-     held_score += dice_number;
-      // $("#activeScore"+active_player).text(held_score);
-  }else{
+    held_score += dice_number;
+    // $("#activeScore"+active_player).text(held_score);
+  } else {
     // TODO: Disable function for roll button when 1 is rolled
-    disableButton($("#roll-dice"),5000);
+    disableButton($("#roll-dice"), 5000);
     alert("You Rolled One");
-    if(active_player === 1){
+    if (active_player === 1) {
       active_player = 2;
-    }else if(active_player === 2){
+    } else if (active_player === 2) {
       active_player = 1;
     }
   }
@@ -50,26 +58,18 @@ if(game_active){
 $(document).ready(function() {
   //click function for "Roll Dice" button
   $("#roll-dice").click(function() {
+    // alert("Roll Dice button Working!");
     $("#diceFaces").show();
-    $(".overlay1").hide();
-    active_player;
-    var dice_number = Math.floor(Math.random()*6)+1;
-    //  alert("Roll dice button Working! You rolled " + dice_face + ".");
-    //  $("#theDice").prepend('<img id="#dice-faces" src="images/Dice/two.png" />')
-    //Below code links Jquery to images
-    // var diceFace = $("#diceFaces").attr("src", "images/Dice/two.png");
-    //Below is links the dice image to the random number.
-    // var diceFace = $("#diceFaces").attr("src", diceimgs[dice_face-1]).appendTo("#diceFaces");
-    // Below code links random number to active current score
-    // $("#activeScore1").text(dice_face);
-    // alert(score1);
+    var dice_number = Math.floor(Math.random() * 6) + 1;
+
     var diceFace = $("#diceFaces").attr("src", diceimgs[dice_number-1]).appendTo("#diceFaces");
 
     if(dice_number !== 1){
        held_score += dice_number;
         // $("#activeScore"+active_player).text(held_score);
     }else{
-      disableButton($("#roll-dice"),5000);
+      rollMessage();
+      disableButton($("#roll-dice"),1000);
       // alert("You Rolled One");
       if(active_player === 1){
         active_player = 2;
@@ -77,21 +77,19 @@ $(document).ready(function() {
         active_player = 1;
       }
     }
-
-
-  })
+  });
   //click function for "New Game" button
-  $("#new-game").click(function () {
+  $("#new-game").click(function() {
     alert("New Game button Working!");
-  })
+  });
   // click function for player1 "Hold" button.
-  $("#hold1").click(function () {
+  $("#hold1").click(function() {
     alert("Hold player1 button Working!");
     active_player === 2;
-  })
+  });
   // click function for player2 "Hold" button.
-  $("#hold2").click(function () {
+  $("#hold2").click(function() {
     alert("Hold player2 button Working!");
     active_player === 1;
-  })
+  });
 });
